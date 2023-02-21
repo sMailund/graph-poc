16bca7afa7b8ee7a10f6572bb2144e3a928ec03f
Simen Mailund Svendsen <sima@netlight.com>
2023-02-21 09:46:45 +0100

    POC microsoft graph

---
+++ graph-poc/MSGraphUser.cs
@@ -0,0 +1,9 @@
+namespace graph_poc;
+
+public class MSGraphUser
+{
+    public string userPrincipalName { get; set; }
+    public string displayName { get; set; }
+    public string mail { get; set; }
+    public string jobTitle { get; set; }
+}
--- graph-poc/Program.cs
+++ graph-poc/Program.cs
@@ -1,8 +1,56 @@
+// https://learn.microsoft.com/en-us/azure/app-service/scenario-secure-app-access-microsoft-graph-as-app?tabs=azure-cli
+
+using System.Net.Http.Headers;
+using Azure.Identity;
+using graph_poc;
+using Microsoft.Graph;
+using WebApplication = Microsoft.AspNetCore.Builder.WebApplication;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// based on https://github.com/Azure-Samples/ms-identity-easyauth-dotnet-storage-graphapi/blob/main/3-WebApp-graphapi-managed-identity/Pages/Graph-MSI/Index.cshtml.cs
+async Task<List<MSGraphUser>> GetUsers()
+{
+    // Create the Graph service client with a ChainedTokenCredential which gets an access
+    // token using the available Managed Identity or environment variables if running
+    // in development.
+    var credential = new ChainedTokenCredential(
+        new ManagedIdentityCredential(),
+        new EnvironmentCredential());
+    var token = credential.GetToken(
+        new Azure.Core.TokenRequestContext(
+            new[] {"https://graph.microsoft.com/.default"}));
+
+    var accessToken = token.Token;
+    var graphServiceClient = new GraphServiceClient(
+        new DelegateAuthenticationProvider((requestMessage) =>
+        {
+            requestMessage
+                .Headers
+                .Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+
+            return Task.CompletedTask;
+        }));
+
+    var msGraphUsers = new List<MSGraphUser>();
+    var users = await graphServiceClient.Users.Request().GetAsync();
+    foreach (var u in users)
+    {
+        MSGraphUser user = new MSGraphUser();
+        user.userPrincipalName = u.UserPrincipalName;
+        user.displayName = u.DisplayName;
+        user.mail = u.Mail;
+        user.jobTitle = u.JobTitle;
+
+        msGraphUsers.Add(user);
+    }
+
+    return msGraphUsers;
+}
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/users", () => "Hello World!");
+app.MapGet("/users", GetUsers);
 
 app.Run();
